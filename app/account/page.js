d9f57e52769a20358f68b3ba62d128b7a697c9f7
mahmoud eid <eid.mahmoud1312@gmail.com>
2025-04-28 09:50:48 +0300

    creating layout

--- app/account/page.js
+++ app/account/page.js
@@ -1,9 +1,7 @@
-import Navigation from "../components/Navigation";
-
 export default function Page() {
   return (
     <div>
-      <Navigation /> <h1>Account Page</h1>{" "}
+      <h1>Account Page</h1>{" "}
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import Navigation from "./components/Navigation";
 
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>hello next</h1>
       <Link href="/cabins">Explore luxury cabins</Link>
     </div>
